ee4523598fc4fc89411929f7a74da11d77805c13
Marcelo Morais <marcelodotmorais@gmail.com>
2018-06-03 01:06:06 -0300

    I2C: testes de erro

--- src/i2c.c
+++ src/i2c.c
@@ -2,18 +2,20 @@
 
 uint32_t error = 0;
 
-#define OLED_ADDRESS                0x3C
-#define OLED_CONTROL_COMMAND_BYTE   0x00
-#define OLED_CONTROL_DATA_BYTE      0x40
+#define OLED_ADDRESS 0x3C
+#define OLED_CONTROL_COMMAND_BYTE 0x80
+#define OLED_CONTROL_DATA_BYTE 0x40
 
-
-void I2C_Init(void) {
+void I2C_Init(void)
+{
 
     //Ativa clock do módulo I2C 0
-    SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R0; 
+    SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R0;
 
     //Espera até módulo 0 estar pronto para uso
-    while((SYSCTL_RCGCI2C_R & SYSCTL_RCGCI2C_R0) != SYSCTL_RCGCI2C_R0) {};
+    while ((SYSCTL_RCGCI2C_R & SYSCTL_RCGCI2C_R0) != SYSCTL_RCGCI2C_R0)
+    {
+    };
 
     //Ativa função de master no módulo 0
     I2C0_MCR_R |= I2C_MCR_MFE;
@@ -27,34 +29,47 @@ void I2C_Oled_send_command(uint8_t command_byte)
 {
 
     //--------Transmission of CONTROL byte--------
-    
+
     //Puts address and transmit bit on the Master Slave Address Register
     I2C0_MSA_R = (OLED_ADDRESS << 1) | 0x00;
 
     //Writes control byte
     I2C0_MDR_R = OLED_CONTROL_COMMAND_BYTE;
+    
+    if (I2C0_MCS_R & I2C_MCS_BUSBSY)
+        error++;
 
     //Writes ---0-011 to Master Control/Status register
-    I2C0_MCS_R = (I2C_MCS_START|I2C_MCS_RUN);
+    I2C0_MCS_R = (I2C_MCS_START | I2C_MCS_RUN);
 
     //Checks Master Control/Status register BUSY bit
-    while((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY);
+    while ((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY)
+    {
+    }
+
+    if (I2C0_MCS_R & I2C_MCS_ERROR)
+        error++;
 
     //--------Transmission of COMMAND byte--------
 
     //Writes command byte
     I2C0_MDR_R = command_byte;
-    
+
     //Writes  ---0-101 to Master Control/Status register
     I2C0_MCS_R = (I2C_MCS_STOP | I2C_MCS_RUN);
 
     //Checks Master Control/Status register BUSY bit
-    while((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY);
+    while ((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY)
+    {
+    }
+
+    if (I2C0_MCS_R & I2C_MCS_ERROR)
+        error++;
 }
 
-void I2C_Oled_send_data(uint8_t data_byte) 
+void I2C_Oled_send_data(uint8_t data_byte)
 {
-    
+
     //--------Transmission of CONTROL byte--------
 
     I2C0_MSA_R = (OLED_ADDRESS << 1) | 0x00;
@@ -63,7 +78,8 @@ void I2C_Oled_send_data(uint8_t data_byte)
 
     I2C0_MCS_R = (I2C_MCS_START | I2C_MCS_RUN);
 
-    while((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY);
+    while ((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY)
+        ;
 
     //--------Transmission of DATA byte--------
 
@@ -71,50 +87,51 @@ void I2C_Oled_send_data(uint8_t data_byte)
 
     I2C0_MCS_R = (I2C_MCS_STOP | I2C_MCS_RUN);
 
-    while((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY);
+    while ((I2C0_MCS_R & I2C_MCS_BUSY) == I2C_MCS_BUSY)
+        ;
 }
 
-void I2C_Oled_Init(void) 
+void I2C_Oled_Init(void)
 {
-    I2C_Oled_send_command(0xAE); // Set display OFF		
- 
+    I2C_Oled_send_command(0xAE); // Set display OFF
+
     I2C_Oled_send_command(0xD5); // Set Display Clock Divide Ratio / OSC Frequency
-    I2C_Oled_send_command(0x80); // Display Clock Divide Ratio / OSC Frequency 
- 
+    I2C_Oled_send_command(0x80); // Display Clock Divide Ratio / OSC Frequency
+
     I2C_Oled_send_command(0xA8); // Set Multiplex Ratio
     I2C_Oled_send_command(0x3F); // Multiplex Ratio for 128x64 (64-1)
- 
+
     I2C_Oled_send_command(0xD3); // Set Display Offset
     I2C_Oled_send_command(0x00); // Display Offset
- 
+
     I2C_Oled_send_command(0x40); // Set Display Start Line
- 
+
     I2C_Oled_send_command(0x8D); // Set Charge Pump
     I2C_Oled_send_command(0x14); // Charge Pump (0x10 External, 0x14 Internal DC/DC)
- 
+
     I2C_Oled_send_command(0xA1); // Set Segment Re-Map
     I2C_Oled_send_command(0xC8); // Set Com Output Scan Direction
- 
+
     I2C_Oled_send_command(0xDA); // Set COM Hardware Configuration
     I2C_Oled_send_command(0x12); // COM Hardware Configuration
- 
+
     I2C_Oled_send_command(0x81); // Set Contrast
     I2C_Oled_send_command(0xCF); // Contrast
- 
+
     I2C_Oled_send_command(0xD9); // Set Pre-Charge Period
     I2C_Oled_send_command(0xF1); // Set Pre-Charge Period (0x22 External, 0xF1 Internal)
- 
+
     I2C_Oled_send_command(0xDB); // Set VCOMH Deselect Level
     I2C_Oled_send_command(0x40); // VCOMH Deselect Level
- 
+
     I2C_Oled_send_command(0xA5); // Set all pixels ON!!!!
     I2C_Oled_send_command(0xA6); // Set display not inverted
     I2C_Oled_send_command(0xAF); // Set display On
-    
+
     I2C_Oled_send_command(0xA5); // Set all pixels ON!!!!
 }
 
-void I2C_Oled_Draw(uint8_t byte) 
+void I2C_Oled_Draw(uint8_t byte)
 {
     I2C_Oled_send_command(SSD1306_COLUMNADDR);
     I2C_Oled_send_command(15);
